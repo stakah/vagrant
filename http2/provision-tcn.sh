816d6719a36e9f8c2e30312d20efc763f18be3e0
Sergio Takahashi <stakah@gmail.com>
2016-04-15 12:28:10 -0300

    Trocando por TCN 1.1.34

--- http2/provision-tcn.sh
+++ http2/provision-tcn.sh
@@ -5,11 +5,12 @@
 echo ${0}
 
 #http://ftp.unicamp.br/pub/apache/tomcat/tomcat-connectors/native/1.2.5/source/tomcat-native-1.2.5-src.tar.gz
+#http://mirror.nbtelecom.com.br/apache/tomcat/tomcat-connectors/native/1.1.34/source/
 
 TCN="tomcat-native"
 TCN_MAJOR_V="1"
-TCN_MINOR_V="2"
-TCN_REV="5"
+TCN_MINOR_V="1"
+TCN_REV="34"
 
 TCN_V=${TCN_MAJOR_V}.${TCN_MINOR_V}.${TCN_REV}
 TCN_VERSION=$TCN-$TCN_V
@@ -20,45 +21,47 @@ TCN_DOWNLOAD_URL="http://ftp.unicamp.br/pub/apache/tomcat/tomcat-connectors/nati
 
 echo JAVA_HOME=$JAVA_HOME
 
-echo installing libssl-dev & subversion & autoconf
+echo "installing libssl-dev, subversion, autoconf"
 sudo apt-get update
-sudo apt-get install libssl-dev subversion autoconf -y
+sudo apt-get install libssl-dev -y
+#sudo apt-get install libssl-dev subversion autoconf -y
 #sudo apt-get install ant
 
 # Destination folder
 DEST=/home/vagrant
 
-#if [ -f /vagrant/$TCN_GZ ]; then 
-#    echo .
-#    echo Copying $TCN_VERSION
-#    cp -f /vagrant/$TCN_GZ $DEST
-#else
-#	echo .
-#	echo Downloading $TCN_GZ
-#    wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  $TCN_DOWNLOAD_URL
-#    cp $TCN_GZ /vagrant
-#fi
-
-#echo .
-#ls $DEST/*.tar.gz
-
-#echo .
-#echo Uncompressing $TCN_GZ
-#tar -xf $TCN_GZ
+if [ -f /vagrant/$TCN_GZ ]; then 
+    echo .
+    echo Copying $TCN_VERSION
+    cp -f /vagrant/$TCN_GZ $DEST
+else
+	echo .
+	echo Downloading $TCN_GZ
+    wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  $TCN_DOWNLOAD_URL
+    cp $TCN_GZ /vagrant
+fi
 
 echo .
-echo Checkin-out source from Subversion
-mkdir $TCN_SRC_FOLDER
-cd $TCN_SRC_FOLDER
-svn co https://svn.apache.org/repos/asf/tomcat/native/trunk/
+ls $DEST/*.tar.gz
+
+echo .
+echo Uncompressing $TCN_GZ
+tar -xf $TCN_GZ
+
+#echo .
+#echo Checkin-out source from Subversion
+#mkdir $TCN_SRC_FOLDER
+#cd $TCN_SRC_FOLDER
+#svn co https://svn.apache.org/repos/asf/tomcat/native/trunk/
 
 # Building $TCN_VERSION
 APR="/usr/local/apr/bin/apr-1-config"
 OPENSSL="/usr/include/openssl"
-cd trunk/native
-autoconf configure.in > configure
-chmod u+x configure
-sh buildconf --with-apr=$APR_SRC
+#cd trunk/native
+cd $TCN_SRC_FOLDER/jni/native
+#autoconf configure.in > configure
+#chmod u+x configure
+#sh buildconf --with-apr=$APR_SRC
 ./configure --with-apr=$APR \
             --with-java-home=$JAVA_HOME \
             --with-ssl=$OPENSSL \
