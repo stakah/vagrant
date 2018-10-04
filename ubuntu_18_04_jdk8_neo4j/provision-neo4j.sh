531f66ebcd779924260f95f27bac1b7815e5dbac
stakah <stakah@gmail.com>
2018-10-04 16:29:03 -0300

    Ajustes no provisionamento do neo4j

--- ubuntu_18_04_jdk8_neo4j/provision-neo4j.sh
+++ ubuntu_18_04_jdk8_neo4j/provision-neo4j.sh
@@ -12,6 +12,15 @@ sudo apt-get update -y
 # installing the latest neo4j community edition
 #sudo apt-get install neo4j
 
-# installing the latest neo4j enterprise edition
-sudo apt-get install neo4j-enterprise -y
+# installing the latest neo4j community edition
+sudo apt-get install neo4j -y
 
+#sudo apt-get install neo4j-enterprise -y
+
+# Configure neo4j to allow connections from outside the VM
+sudo sed -ibk -f /vagrant/script.sed /etc/neo4j/neo4j.conf
+
+# Configure neo4j service to restart when rebooting or crashing
+sudo systemctl enable neo4j.service
+sudo systemctl daemon-reload
+sudo systemctl restart neo4j.service
---
+++ ubuntu_18_04_jdk8_neo4j/script.sed
@@ -0,0 +1,5 @@
+s/#dbms.connectors.default_listen_address=0.0.0.0/dbms.connectors.default_listen_address=0.0.0.0/
+s/#dbms.connector.bolt.listen_address=:7687/dbms.connector.bolt.listen_address=:7687/
+s/#dbms.connector.http.listen_address=:7474/dbms.connector.http.listen_address=:7474/
+s/#dbms.connector.https.listen_address=:7473/dbms.connector.https.listen_address=:7473/
+
