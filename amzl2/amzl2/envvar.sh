783b8ff3fe871d8758a3fbcb8ea0ae6c1c764e61
stakah <stakah@gmail.com>
2019-07-30 00:06:47 -0300

    Vagrantfile amazon linux2 mysqldb, python3, ionic 5, angular 8 (update)

--- amzl2/amzl2/envvar.sh
+++ amzl2/amzl2/envvar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # root password
-export AMZL2_MYSQL_ROOT_PASSWD='BC4$ports'
+export AMZL2_MYSQL_ROOT_PASSWD='S1crTPas$w0rd'
 
 # used to create the database instance and user
-export AMZL2_MYSQL_DB=bc4db
-export AMZL2_MYSQL_USER=bc4user
-export AMZL2_MYSQL_PASSWD='BC4$ports'
+export AMZL2_MYSQL_DB=amzolx2db
+export AMZL2_MYSQL_USER=amzolx2user
+export AMZL2_MYSQL_PASSWD='Pas$w0rd'
